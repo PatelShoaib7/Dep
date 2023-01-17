213bf8bfbd739e6857be67a41978f8926b7d01e4
Shoaib Patel <patelshoaib125@gmail.com>
2023-01-17 19:24:44 +0530

    final commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { urlencoded } = require('express');
 const express = require('express');
+require('dotenv').config();
  const cors = require('cors');
  const connection = require('./db')
  const jwt =  require("jsonwebtoken");
- const secret_key="avshN^*F259HxksJP55(2.)&YFCs89$^%&**(HB"
+ const secret_key=process.env.SECRET_KEY
  const userMOdel = require("./MODELS/userModel")
- require('dotenv').config();
 const  app = express();
 app.use(express.json())
  app.use(express.text())
@@ -14,31 +14,37 @@ app.use(urlencoded({extended:true}))
  
 
 
-app.get('/', (req, res)=>{
-    res.send("Welcome to depeloyed apge") 
-})
+        app.get('/', (req, res)=>{
+            res.send("Welcome to depeloyed apge") 
+        })
+         app.post("/login", async (req , res)=>{
+            const {username , email} = req.body;
+           const user =  await   userMOdel.find({$or:[{email:email} , {username:username}]});
+             console.log(user)
+            const jwt_token = jwt.sign({user} , secret_key ) 
+            if(user.length>=1){
+                    res.status(201).send({message :"login sucessful",token:jwt_token})
+            }else{
+              res.status(401).send({message: 'oops user is not registred please sign up first'})
+            }
+        })
+        app.post("/signup", async (req, res)=>{
+            const user =  await   userMOdel( req.body)
+            user.save((err , sucess)=>{
+                    if(err){
+                        res.status(500).send({message:'oops something went wrong plz try again'})
+                    }
 
-
-
-   app.post("/signup", async (req, res)=>{
-         const  dataFromClient = req.body;
-    const user =  await   userMOdel(dataFromClient)
-    user.save((err , sucess)=>{
-       if(err){
-        console.log(err)
-       }
-       res.status(200).send({message : "data savavd to data bases"})
-    })
-       console.log("data saved ")
-   })
-
-app.listen(PORT, async ()=>{
-     try{
-       await connection;
-       console.log("conneted to data base")
-     }catch(err){
-           console.log("connetion data base error")
-          console.log(err)
-        } 
-    console.log(`Suvcessfully Runnig On ${PORT}`)
-  })
+                    res.status(200).send({message : "sign up sucessful"})
+            })
+        })
+        app.listen(PORT, async ()=>{
+            try{
+            await connection;
+            console.log("conneted to data base")
+            }catch(err){
+                console.log("connetion data base error")
+                console.log(err)
+                } 
+            console.log(`Suvcessfully Runnig On ${PORT}`)
+        })

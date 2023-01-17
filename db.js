17d2c6dcfa8a24682b6f54bddf53e548ae760626
Shoaib Patel <patelshoaib125@gmail.com>
2023-01-17 19:26:07 +0530

    final commit

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose")
 require('dotenv').config();
-//const connection =  mongoose.connect(`mongodb+srv://12345:3zhqr%uVxS*qNWK@cluster0.g2n4bjn.mongodb.net/ZOMATO_USERS?retryWrites=true&w=majority`);
- const connection = mongoose.connect(process.env.MONGO_URL);
+const connection = mongoose.connect(process.env.MONGO_URL);
 
 module.exports = connection
